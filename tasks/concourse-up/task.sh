ae16f3b507cb335ee3f8e658e3bb88fed6acbe6c
Aaron Gershman <aegershman@gmail.com>
2018-09-17 23:43:36 -0500

    cleanup params

--- tasks/concourse-up/task.sh
+++ tasks/concourse-up/task.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
 
-set -eu
-[ 'true' = "${DEBUG:-}" ] && set -x
-
 chmod +x concourse-up/concourse-up-linux*
 concourse_up=$(pwd)/concourse-up/concourse-up-linux*
 
 case "$COMMAND" in
-deploy)
-	$concourse_up deploy "$PROJECT_NAME"
-	;;
-
-destroy)
-	$concourse_up destroy "$PROJECT_NAME" <<<"yes"
-	;;
-
-info)
-	$concourse_up info "$PROJECT_NAME"
-	;;
+deploy) $concourse_up deploy "$PROJECT_NAME" ;;
+destroy) $concourse_up destroy "$PROJECT_NAME" <<<"yes" ;;
+info) $concourse_up info "$PROJECT_NAME" ;;
 
 *)
-	echo "COMMAND is invalid"
+	echo "command $COMMAND is unknown"
 	exit 1
 	;;
 esac
